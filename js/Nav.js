02357b861e818ef0f7d60486263e72e18c22cdcb
Luca Megens <mail@lucamegens.nl>
2024-09-28 17:48:40 +0200

    added navigation animations

---
+++ js/Nav.js
@@ -0,0 +1,9 @@
+function OpenNav(){
+    document.getElementById("Nav-isOpen").style.display="flex";
+    document.getElementById("Nav-isClose").style.display="none";
+};
+
+function CloseNav(){
+    document.getElementById("Nav-isOpen").style.display="none";
+    document.getElementById("Nav-isClose").style.display="flex";
+};
